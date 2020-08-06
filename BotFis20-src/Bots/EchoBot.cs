7bcccb9f58ef62237f52441dd05ae6313b420683
sebastianxdxd192 <69281564+sebastianxdxd192@users.noreply.github.com>
2020-08-05 21:41:53 -0500

    Tercera Version 3.0
    Tercera Version 3.0

--- BotFis20-src/Bots/EchoBot.cs
+++ BotFis20-src/Bots/EchoBot.cs
@@ -19,7 +19,7 @@ namespace Microsoft.BotBuilderSamples.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = $"Hola y Bienvenido!";
+            var welcomeText = $"Hola Innovador!";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
